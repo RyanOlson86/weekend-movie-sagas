b7f2159a49ad7179aefe5a0a161f73f7db166ba9
Ryan Olson <ryan.olson86@yahoo.com>
2024-01-27 15:32:38 -0600

    Completed styling for details page

--- src/components/DetailsPage/DetailsPage.jsx
+++ src/components/DetailsPage/DetailsPage.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
-import { Card, CardContent, Button, CardActions, Typography, Accordion, AccordionDetails, AccordionSummary } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  Button,
+  CardActions,
+  Typography,
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Divider,
+  Box,
+} from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import { Margin } from "@mui/icons-material";
 
 function DetailsPage() {
   const history = useHistory();
@@ -42,40 +52,47 @@ function DetailsPage() {
       <button data-testid="toList" onClick={handleClick}>
         HOME
       </button> */}
-      <Card inputProps={{"data-testid":"movieDetails"}} >
+      <Card inputProps={{ "data-testid": "movieDetails" }} variant="outlined" sx={{margin: "30px"}}>
         <CardContent
           sx={{
-            width: "500px",
+            width: "auto",
             display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
+            flexDirection: "row",
+            alignItems: "flex-start",
+            paddingTop: "10px",
+            paddingBottom: "10px"
           }}
         >
-          <Typography variant="h4" gutterBottom textAlign="center">
-            {movieDetails?.title}
-          </Typography>
+          <Box sx={{width: "400px", textAlign: "center"}}>
+            <Typography variant="h4" gutterBottom  sx={{width: "400px"}}>
+              {movieDetails?.title}
+            </Typography>
 
-          <img src={movieDetails?.poster} />
+            <img src={movieDetails?.poster} style={{ width: "350px"}} />
+          </Box>
 
-          <Accordion sx={{margin: "10px"}}>
-            <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1-content" id="panel1-header" sx={myStyle}>
-              Genres
-            </AccordionSummary>
-            <AccordionDetails>
-              {genres.map((genre, i) => (
-                <li key={i}>{genre.name}</li>
-              ))}
-            </AccordionDetails>
-          </Accordion>
+          <Divider orientation="vertical" variant="fullWidth" flexItem />
 
-          <Accordion>
-            <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1-content" id="panel1-header" sx={myStyle}>
-              Description
-            </AccordionSummary>
-            <AccordionDetails>{movieDetails?.description}</AccordionDetails>
-          </Accordion>
+          <Box>
+            <Accordion sx={{ margin: "10px"}}>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1-content" id="panel1-header" sx={myStyle}>
+                Genres
+              </AccordionSummary>
+              <AccordionDetails>
+                {genres.map((genre, i) => (
+                  <li key={i}>{genre.name}</li>
+                ))}
+              </AccordionDetails>
+            </Accordion>
 
-          {/* <Box component="form">
+            <Accordion sx={{ margin: "10px" }}>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1-content" id="panel1-header" sx={myStyle}>
+                Description
+              </AccordionSummary>
+              <AccordionDetails>{movieDetails?.description}</AccordionDetails>
+            </Accordion>
+
+            {/* <Box component="form">
             <TextField
               label={title}
               variant="outlined"
@@ -85,11 +102,12 @@ function DetailsPage() {
               onChange={(event) => setFormInput(event.target.value)}
             ></TextField>
           </Box> */}
-          <CardActions>
-            <Button size="large" variant="contained" onClick={handleClick} data-testid="toList" sx={{margin: "10px"}}>
-              HOME
-            </Button>
-          </CardActions>
+            <CardActions>
+              <Button size="large" variant="contained" onClick={handleClick} data-testid="toList" sx={{ margin: "10px" }}>
+                HOME
+              </Button>
+            </CardActions>
+          </Box>
         </CardContent>
       </Card>
     </div>
