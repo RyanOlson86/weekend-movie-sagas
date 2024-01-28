c470ebc80a8b60faf4f3290384e009b76e101441
Ryan Olson <ryan.olson86@yahoo.com>
2024-01-27 22:28:54 -0600

    Added links to header and cancel button functionality to AddMovie

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,24 @@
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import React from "react";
-
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    const history = useHistory();
+  const history = useHistory();
 
-    // Clicking on header title will send back to home
-    const handleClick = () => {
-        history.push("/");
-      };
+  // Clicking on header title will send back to home
+  const handleClick = () => {
+    history.push("/");
+  };
 
-    return (
-        <div className="header">
-            <h1 onClick={handleClick}>Show Me The Movies</h1>
-        </div>
-    )
-}
+  return (
+    <div className="header">
+      <h1 onClick={handleClick}>Show Me The Movies</h1>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/add-movie">Add Movie</Link>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
