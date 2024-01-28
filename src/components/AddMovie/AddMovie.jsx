ead960fae8db62165f8d989e741ba62e7593f612
Ryan Olson <ryan.olson86@yahoo.com>
2024-01-28 11:45:38 -0600

    Finished styling AddMovie and Header

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Box, TextField, Typography, FormControl, InputLabel, Select, MenuItem, Button } from "@mui/material";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import "./AddMovie.css";
 
 const genres = [
   {
@@ -96,41 +97,63 @@ const AddMovie = () => {
   };
 
   const handleCancel = () => {
-    history.push('/')
-  }
+    history.push("/");
+  };
 
   return (
     <Box
       component="form"
       sx={{
-        "& > :not(style)": { m: 1, width: "25ch" },
+        "& > :not(style)": { m: 2 },
         backgroundColor: "white",
+        width: "80%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
       }}
       noValidate
       autoComplete="off"
     >
-      <Typography variant="h5">Add a movie to the database: </Typography>
-      <TextField id="outlined-basic" label="Movie Title" variant="outlined" value={titleInput} onChange={handleTitle} />
-      <TextField id="outlined-basic" label="Image URL" variant="outlined" value={imgInput} onChange={handleImg} />
+      <Typography variant="h4">Add a movie to the database: </Typography>
+      <Box className="inputs">
+        <TextField id="movie-title" label="Movie Title" variant="outlined" value={titleInput} onChange={handleTitle} />
+        <TextField id="url-path" label="Image URL" variant="outlined" value={imgInput} onChange={handleImg} />
+      </Box>
 
-      <FormControl fullWidth>
-        <InputLabel>Genre</InputLabel>
-        <Select value={genreId} label="Genre" onChange={handleGenre}>
-          {genres.map((genre) => (
-            <MenuItem key={genre.id} value={genre.id}>
-              {genre.name}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+      <Box sx={{ width: "90%" }}>
+        <FormControl fullWidth>
+          <InputLabel>Genre</InputLabel>
+          <Select value={genreId} label="Genre" onChange={handleGenre}>
+            {genres.map((genre) => (
+              <MenuItem key={genre.id} value={genre.id}>
+                {genre.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Box>
 
-      <TextField id="outlined-basic" label="Description" variant="outlined" value={descInput} onChange={handleDescription} />
-      <Button variant="contained" color="secondary" onClick={handleCancel}>
-        CANCEL
-      </Button>
-      <Button variant="contained" onClick={handleSubmit}>
-        SUBMIT
-      </Button>
+      <Box sx={{ width: "90%" }}>
+        <TextField
+          id="desc-input"
+          label="Description"
+          variant="outlined"
+          value={descInput}
+          onChange={handleDescription}
+          fullWidth
+          multiline
+          rows={6}
+        />
+      </Box>
+
+      <Box>
+        <Button variant="contained" color="secondary" onClick={handleCancel}>
+          CANCEL
+        </Button>
+        <Button variant="contained" onClick={handleSubmit}>
+          SUBMIT
+        </Button>
+      </Box>
     </Box>
   );
 };
